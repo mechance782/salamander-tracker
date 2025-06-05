8e790a334d0c729cf49fb4c2f954b2ec6493cb69
Kimberly Mageary <kimberlymageary@gmail.com>
2025-06-04 20:58:43 -0400

    Added mui styling for our nav bar (basic no coloring yet)

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,14 @@
 import Link from "next/link"
+import { AppBar, Toolbar, Button} from '@mui/material';
 
 export default function Navigation(){
     return (<>
-        <nav>
-            <Link href="/" className="nav-link">Home</Link>
-            <Link href="/videos">Available Videos</Link>
-            <Link href="/preview/no-video">Process Video</Link>
-        </nav>
+        <AppBar position ="static">
+            <Toolbar>
+                <Link href="/" className="nav-link"><Button color="inherit">Home</Button></Link>
+                <Link href="/videos"><Button color="inherit">Available Videos</Button></Link>
+                <Link href="/preview/no-video"><Button color="inherit">Process Video</Button></Link> 
+            </Toolbar>
+        </AppBar>
     </>)
 }
