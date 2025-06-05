858d7404d5cd27226b24ae179c39eab584a5ae8d
Kimberly Mageary <kimberlymageary@gmail.com>
2025-06-04 21:33:25 -0400

    Fixed the navigation margins

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import { AppBar, Toolbar, Button} from '@mui/material';
 
 export default function Navigation(){
     return (<>
-        <AppBar position ="static">
+        <AppBar position ="static" sx={{mb:2}}>
             <Toolbar>
                 <Link href="/" className="nav-link"><Button color="inherit">Home</Button></Link>
                 <Link href="/videos"><Button color="inherit">Available Videos</Button></Link>
