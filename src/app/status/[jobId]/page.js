9d92d4633133c47d800c0631b6763cdcb64c6df9
Madeleine Chance <mechance782@gmail.com>
2025-06-10 18:39:32 -0700

    Working on job status page

--- src/app/status/[jobId]/page.js
+++ src/app/status/[jobId]/page.js
@@ -1,27 +1,83 @@
 "use client"
-import { useEffect, useRef } from "react"
+import {Grid, Card, Typography, Divider, Button, IconButton, Tooltip} from '@mui/material'
+import RefreshIcon from '@mui/icons-material/Refresh';
+import Link from 'next/link';
+import { useEffect, useRef, useState } from "react"
 
 export default function Status({ params }) {
-    const hasFetched = useRef(false);
+    const [thisJobId, setThisJobId] = useState(null)
+    const [jobStatus, setJobStatus] = useState('getting job status...')
+    const [jobError, setJobError] = useState(null)
+    const [jobResult, setJobResult] = useState(null)
+    const [statusColor, setStatusColor] = useState('primary.dark')
+    const [csvUrl, setCsvUrl] = useState(null)
+    const [refresh, setRefresh] = useState(false);
+    const [loading, setLoading] = useState(false);
+
 
     useEffect(() => {
-        if (!hasFetched.current) {
-            hasFetched.current = true;
-            fetchJobStatus();
-        }
-    }, []);
+        fetchJobStatus()
+    }, [refresh]);
 
     const fetchJobStatus = async () => {
         try {
             const { jobId } = await params; 
             const response = await fetch(`http://localhost:3000/process/${jobId}/status`);
+            setThisJobId(jobId);
 
             const data = await response.json();
+            if (data.error){
+                setJobStatus('Error')
+                setJobError(data.error)
+                setStatusColor('error')
+            } else {
+                setJobStatus(data.status)
+                if (data.result){
+                    const resultLink = 'http://localhost:3000' + data.result;
+                    await handleDownload(resultLink);
+                    setJobResult(data.result)
+                    setStatusColor('success')
+                    
+                }
+            }
+
+            setLoading(false);
+            
             console.log(data);
         } catch (error) {
             console.log('error fetching job status:' + error);
         }
     };
 
-    return (<></>);
+    // used this post as source for info on link downloads in react:
+    // https://www.geeksforgeeks.org/reactjs/how-to-download-pdf-file-in-reactjs/
+    const handleDownload = async (link) => {
+        const data = await fetch(link);
+        const blob = await data.blob();
+        const csvUrl = window.URL.createObjectURL(blob);
+        setCsvUrl(csvUrl);
+    };
+
+    const handleRefresh = () => {
+        setLoading(true)
+        setRefresh(prev => !prev)
+        
+    };
+
+    return (<>
+        <Card sx={{padding: 2}}>
+                <Typography component='h1' variant='h6'>Overview of Job <Typography component='span' color='primary.dark'>{thisJobId}</Typography>:</Typography>
+                <Divider />
+                <Grid sx={{margin: 2}}>
+
+                    <Typography color={statusColor}><Typography color='textPrimary' component='span' sx={{fontWeight: "bold"}}>Status:</Typography> {jobStatus}
+                     {jobStatus == 'processing' ? (<>... <Tooltip title="Refresh"><IconButton onClick={handleRefresh} loading={loading}><RefreshIcon /></IconButton></Tooltip></>): (<></>)}</Typography>
+
+                    {jobResult ? (<><Typography sx={{fontWeight: "bold"}}>Result:<Link download href={csvUrl}><Button>{jobResult}</Button></Link></Typography></>):(<></>)}
+                    {jobError ? (<Typography color={statusColor}>*{jobError}</Typography>) : (<></>)}
+                    
+                </Grid>
+        </Card>
+
+    </>);
 }
